e8a9936a3255b8f44f7b3177823a96d3722e73dd
russoren1 <russoren@students.zhaw.ch>
2026-09-22 13:56:46 +0200

    added country and state

--- src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
@@ -1,15 +1,19 @@
 package ch.zhaw.avalanger.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/avalange")
 public class AvalangeController {
 
-    @GetMapping("")
-    public String getAllAvelanges() {
-        return "No avelanges found..";
-    } 
+    @GetMapping({ "", "/{country}" }) // Pfadvariable hinzufügen, Lawinen nach Ländern filtern
+    public String getAllAvelanges(@PathVariable(required = false) String country,
+            @RequestParam(required = false) String state) {
+        return "No avelanges found for " + country + ", state: " + state;
+    }
+
 }
